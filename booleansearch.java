90011e1dbe52fb9c10028178e6224a9824e6d799
Sahil Deshmukh <89685991+sahildeshmukh45@users.noreply.github.com>
2024-09-04 09:56:12 +0530

    Binary search with some leetcode solved questions

---
+++ booleansearch.java
@@ -0,0 +1,12 @@
+//
+//
+//import java.util.Arrays;
+//
+//public class booleansearch {
+//    public static void main(String[] args) {
+//
+//    }
+//    static int[] search(int[]a, int target) {
+//
+//    }
+//}
